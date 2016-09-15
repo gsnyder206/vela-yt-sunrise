f1a4daeb48445a904b5742a3b7e2145e872189e2
RaymondSimons <rsimons@pha.jhu.edu>
2016-09-15 13:41:16 -0400

    fix seed

--- genSunriseInput.py
+++ genSunriseInput.py
@@ -105,11 +105,13 @@ def generate_cameras(normal_vector, seed = 0, distance=100.0, fov=50.0, mov_ang 
     ts_random = np.random.random(segments_random)*np.pi*2
     #ps_random = np.random.random(segments_random)*np.pi-np.pi/2.0
     ps_random = np.random.random(segments_random)*np.pi
+    ps_random = np.acos(2*np.random.random(segments_random)-1)
 
     np.random.seed(seed)
     ts_fixed = np.random.random(segments_fixed)*np.pi*2
     #ps_fixed = np.random.random(segments_fixed)*np.pi-np.pi/2.0
     ps_fixed = np.random.random(segments_fixed)*np.pi
+    ps_fixed = np.acos(2*np.random.random(segments_fixed)-1)
 
 
 
