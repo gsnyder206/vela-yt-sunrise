0330331c0ba3b56369be835e191c7ddb338446d1
RaymondSimons <rsimons@pha.jhu.edu>
2016-12-02 12:09:39 -0500

    temp debug change cameras

--- genSunriseInput.py
+++ genSunriseInput.py
@@ -92,7 +92,7 @@ def generate_cameras(normal_vector, seed = 0, distance=100.0, fov=50.0, mov_ang 
             ['face',([0.,0.,1.],[0.,-1.,0],True)], #up is north=+y
             ['edge',([0.,1.,0.],[0.,0.,-1.],True)],#up is along z
             ])
-            '''
+    '''
             ['backface',([0.,0.,-1.],[0.,-1.,0],True)], #up is north=+y
             ['backedge',([0.,-1.,0.],[0.,0.,-1.],True)],#up is along z
             ['45',([0.,0.7071,0.7071],[0., 0., -1.],True)],
@@ -100,9 +100,9 @@ def generate_cameras(normal_vector, seed = 0, distance=100.0, fov=50.0, mov_ang 
             ['Y-axis',([0.,1.,0.],[0.,0.,-1.],False)],#up is along z
             ['X-axis',([1.,0.,0.],[0.,0.,-1.],False)],#up is along z
             ])  
-            '''
 
-    '''
+
+
     segments_fixed  = 4
     segments_random = 7
 
@@ -139,10 +139,10 @@ def generate_cameras(normal_vector, seed = 0, distance=100.0, fov=50.0, mov_ang 
             camera_set['Random_%03i'%(i-segments_fixed)]=(pos,vc,False)
 
     if movie: camera_set['Movie_angle'] = ([0.,math.cos(mov_ang*180./pi),math.sin(2*180./pi)],[0.,0.,-1.],False)
+    '''
 
 
 
-    '''  
 
     i=0  
     cameras = OrderedDict()
