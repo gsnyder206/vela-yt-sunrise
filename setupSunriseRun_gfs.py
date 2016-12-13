255c2f890aec11b68bf38064a3ab1a048afa89f4
gsnyder206 <snyder.gregoryf@gmail.com>
2016-12-13 09:09:22 -0500

    runtime update

--- setupSunriseRun_gfs.py
+++ setupSunriseRun_gfs.py
@@ -212,7 +212,7 @@ if __name__ == "__main__":
     model='has'      #options are 'wes', 'san', 'ivy', 'has', in increasing goodness and expense
     queue='normal'   #options devel, debug, low, normal, long
     notify='gsnyder@stsci.edu'
-    walltime_limit='07:00:00'
+    walltime_limit='08:00:00'
 
     stub_dir = '/u/gfsnyder/sunrise_data/stub_files'
 
