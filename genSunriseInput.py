f8c0d27cd2e54f58873ac52b8a4971d40baf0f45
gsnyder206 <snyder.gregoryf@gmail.com>
2016-12-14 08:28:06 -0500

    turning off --no_gas_p in export case

--- genSunriseInput.py
+++ genSunriseInput.py
@@ -229,7 +229,7 @@ def write_qsub_exporters(snapname,qsubfn,aname):
     qsfo.write('#PBS -e sunrise_export_'+aname+'pbs.err\n')
     qsfo.write('#PBS -V\n\n')
     
-    qsfo.write('python $VELAYTSUNRISE_CODE/genSunriseInput.py '+os.path.basename(snapname)+' --no_gas_p > export_test_'+aname+'.out 2> export_test_'+aname+'.err\n')
+    qsfo.write('python $VELAYTSUNRISE_CODE/genSunriseInput.py '+os.path.basename(snapname)+' > export_test_'+aname+'.out 2> export_test_'+aname+'.err\n')
     qsfo.close()
     
 
