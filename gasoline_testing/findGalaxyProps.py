644c904d1c05c41f0cd7ccc14b460175d218774d
RaymondSimons <rsimons@pha.jhu.edu>
2017-08-14 15:11:44 -0700

    change filename

--- gasoline_testing/findGalaxyProps.py
+++ gasoline_testing/findGalaxyProps.py
@@ -440,7 +440,8 @@ if __name__ == "__main__":
                                 'omega_lambda': 0.728,
                                 'omega_matter': 0.272,
                                 'hubble_constant': 0.702}
-        ds = yt.load(ts[0], cosmology_parameters=cosmology_parameters)
+        print new_snapfiles
+        ds = yt.load(new_snapfiles, cosmology_parameters=cosmology_parameters)
         #for ds,snap_dir in zip(reversed(ts, cosmology_parameters=cosmology_parameters),np.flipud(new_snapfiles)):
 
         dd = ds.all_data()
