603b0eddfb269add4696884ec73adb559f0d0e30
RaymondSimons <rsimons@jhu.edu>
2018-09-12 13:10:27 -0400

    default testing message

--- enzoGalaxyProps_rcs.py
+++ enzoGalaxyProps_rcs.py
@@ -10,12 +10,8 @@ import findGalaxyProps as fGP
 
 
 if __name__=="__main__":
-    if len(sys.argv)==3:
-        snaps = np.asarray([sys.argv[1]])+'/' +np.asarray([sys.argv[1]])
-        form= sys.argv[2]
-    else:
-        snaps = np.ssort(np.asarray(glob.glob("RD????/RD????")))  #ENZO format a list of snapshots in separate directories
-        form='ENZO'
+    snaps = np.sort(np.asarray(glob.glob("RD????/RD????")))  #ENZO format a list of snapshots in separate directories
+    form='ENZO'
 
 
     print snaps, form
