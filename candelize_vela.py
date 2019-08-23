2a867321b6d8642cc39472832d3b328225d512bd
gsnyder206 <gsnyder@stsci.edu>
2019-08-23 12:04:11 -0400

    bug fix

--- candelize_vela.py
+++ candelize_vela.py
@@ -279,7 +279,7 @@ def process_snapshot(subdirpath='.',mockimage_parameters=None,clobber=False, max
             raise
         except:
             print("Exception while processing broadband: ", bbfile)
-            print("Error:", sys.exc_info()[0])
+            print("Error:", sys.exc_info())
         else:
             print("Successfully processed broadband: ", bbfile)
 
