80e95e270e5d31b93a3aaf045641a75f3097fff5
gsnyder206 <gsnyder@stsci.edu>
2020-05-15 08:54:54 -0400

    check centers

--- check_centers.py
+++ check_centers.py
@@ -11,12 +11,35 @@ if __name__=="__main__":
     print(simname)
 
     pd=np.load(pf).all()
-    tc=pd['true_center']  #in physical kpc
+    tc=pd['true_center']  #in physical kpc, a list of arrays
     print(tc)
-
-    #read ceverino centers file and parse into arrays
+    a=pd['scale']
+    x=np.zeros_like(a)
+    y=np.zeros_like(a)
+    z=np.zeros_like(a)
 
     #convert galprops centers to comoving
+    for i,scale,cen in enumerate(zip(a,tc)):
+        print('{:5.2f} {:10.3f}'.format(scale, cen[0]*(0.70)/scale))
+        x[i]=cen[0]*0.70/scale
+        y[i]=cen[1]*0.70/scale
+        z[i]=cen[2]*0.70/scale
+
+    #read ceverino centers file and parse into arrays
+    dcf='~/vela_data/ceverino_centers_gen6.txt'
+    #VELA01/profileSpGe_Reca0.200.dat:# (xc,yc,zc)[Mpc h-1 comoving]=    5.1572    5.0681    4.3615
+    dcfo=open(dcf)
+    lines=dcfo.readlines()
+    this_lines=[]
+    for l in lines:
+        line_sim=l[0:6]
+        if line_sim==simname:
+            this_lines.append(l)
+
+    print(this_lines)
+
+    dcfo.close()
+
 
     #measure distance with ceverino centers
 
