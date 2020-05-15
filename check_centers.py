913ee9218d641896d4affab2f5a5dd4d086e9faf
gsnyder206 <gsnyder@stsci.edu>
2020-05-15 09:07:54 -0400

    check centers

--- check_centers.py
+++ check_centers.py
@@ -18,7 +18,7 @@ if __name__=="__main__":
     y=np.zeros_like(a)
     z=np.zeros_like(a)
 
-    #convert galprops centers to comoving
+    #convert galprops centers to comoving kpc
     for i,(scale,cen) in enumerate(zip(a,tc)):
         print('{:5.2f} {:10.3f}'.format(scale, cen[0]*(0.70)/scale))
         x[i]=cen[0]*0.70/scale
@@ -42,9 +42,9 @@ if __name__=="__main__":
             ss=l[23:28]
             dc_a.append(np.float64(ss))
             nums=l[-28:-1]
-            dc_x.append(np.float64(nums.split('   ')[0]))
-            dc_y.append(np.float64(nums.split('   ')[1]))
-            dc_z.append(np.float64(nums.split('   ')[2]))
+            dc_x.append(1000*np.float64(nums.split('   ')[0]))
+            dc_y.append(1000*np.float64(nums.split('   ')[1]))
+            dc_z.append(1000*np.float64(nums.split('   ')[2]))
     dcfo.close()
 
     dc_x=np.asarray(dc_x)
@@ -52,9 +52,15 @@ if __name__=="__main__":
     dc_z=np.asarray(dc_z)
     dc_a=np.asarray(dc_a)
 
-    for a,dx in zip(dc_a,dc_x): print(a,dx)
+    #for a,dx in zip(dc_a,dc_x): print(a,dx)
 
     #measure distance with ceverino centers
+
+    for scale,truecen in zip(z,tc):
+        #match to dc_a
+        dc_i=dc_a==scale
+        print(np.sum(dc_i))
+
 
 
     #print out distances versus scalefactor
