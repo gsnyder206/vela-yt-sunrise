faa3cbb027020a4281abfca5cee77e4c985df627
gsnyder206 <gsnyder@stsci.edu>
2021-01-12 13:48:45 -0500

    omit update_star_radius

--- genSunriseInput.py
+++ genSunriseInput.py
@@ -1,7 +1,7 @@
 '''
 Working from Miguel Rocha's script: genSunriseInput.py. This script
 generates the cameras to use in Sunrise and makes some projection plots for
-a few of the cameras. 
+a few of the cameras.
 
 '''
 import numpy as np
@@ -18,20 +18,20 @@ import update_star_radius
 def parse():
     '''
     Parse command line arguments
-    ''' 
+    '''
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
                                      description='''\
                                 Generate the cameras to use in Sunrise and make projection plots
                                 of the data for some of these cameras. Then export the data within
                                 the fov to a FITS file in a format that Sunrise understands.
                                 ''')
- 
+
     parser.add_argument('snap_files', nargs='?', default=None, help='Snapshot files to be analyzed.')
 
     parser.add_argument('segments_random', nargs='?', default=None, help='Number of random cameras.')
 
     #parser.add_argument('-s', '--snap_base', default='10MpcBox_csf512_',
-    #                    help='Base of the snapshots file names.') 
+    #                    help='Base of the snapshots file names.')
 
     parser.add_argument('-d', '--distance', default=100000, type=float,
                         help='Distance between cameras and the center of the galaxy (in [kpc]).')
@@ -41,18 +41,18 @@ def parse():
     #                    help='Field of view of the cameras at the image plane (in [kpc]).')
 
     parser.add_argument('--format',default='VELA',type=str,
-                        help='Simulation type (ENZO or VELA)') 
-    
-    
+                        help='Simulation type (ENZO or VELA)')
+
+
     #I think many of these aren't needed for now.
     '''
     parser.add_argument('--star_particles', default='stars',
                         help='The name given to the star particles in the yt dataset '\
-                        '(as printed in ds.field_list or ds.derived_field_list).') 
+                        '(as printed in ds.field_list or ds.derived_field_list).')
 
     parser.add_argument('--dm_particles', default='darkmatter',
                         help='The name given to the dark matter particles in the yt dataset '\
-                        '(as printed in ds.field_list or ds.derived_field_list).') 
+                        '(as printed in ds.field_list or ds.derived_field_list).')
 
     parser.add_argument( '--galprops_file', default='sim_dir/analysis/catalogs/*_galaxy_props.npy',
                         help='File containing the galaxy properties. A python dictionary is expected '\
@@ -66,24 +66,24 @@ def parse():
 
     parser.add_argument('--out_dir',default='sim_dir/analysis/sunrise_analysis/',
                         help='Directory where the output will be placed. A sub directory will be created '\
-                            'for each snapshot') 
+                            'for each snapshot')
 
     parser.add_argument('--rockstar_out_dir', default='sim_dir/analysis/rockstar_output/',
                         help='Directory where to find the rockstar output, used to annotate halos '\
                             'on plots. If not found or set to None halos will not be annotated.')
     '''
     #parser.add_argument('--no_plots',action='store_true',
-    #                    help='Do not generate projection plots.') 
+    #                    help='Do not generate projection plots.')
 
     parser.add_argument('--no_export',action='store_true',
-                        help='Do not export data to fits for Sunrise.') 
+                        help='Do not export data to fits for Sunrise.')
 
     parser.add_argument('--no_gas_p',action='store_true',
                         help='Shut off momentum of gas grid.')
-    
+
     parser.add_argument('--email',default='gsnyder@stsci.edu',type=str,
                         help='email address for job notifications')
-    
+
 
     args = vars(parser.parse_args())
     return args
@@ -97,7 +97,7 @@ def generate_cameras(normal_vector, seed = 0, distance=100.0, fov=50.0, mov_ang 
     from yt.utilities.orientation import Orientation
 
     print( "\nGenerating cameras")
-    
+
     north = np.array([0.,1.,0.])
     orient = Orientation(normal_vector=normal_vector, north_vector=north)
     R=np.linalg.inv(orient.inv_mat)
@@ -110,7 +110,7 @@ def generate_cameras(normal_vector, seed = 0, distance=100.0, fov=50.0, mov_ang 
             ['Z-axis',([0.,0.,-1.],[0.,-1.,0],False)], #up is north=+y
             ['Y-axis',([0.,1.,0.],[0.,0.,-1.],False)],#up is along z
             ['X-axis',([1.,0.,0.],[0.,0.,-1.],False)],#up is along z
-            ])  
+            ])
 
 
     np.random.seed()
@@ -136,7 +136,7 @@ def generate_cameras(normal_vector, seed = 0, distance=100.0, fov=50.0, mov_ang 
         print( theta, phi)
         pos = [np.cos(theta)*np.sin(phi),np.sin(theta)*np.sin(phi),np.cos(phi)]
         vc = [np.cos(np.pi/2 - theta)*np.sin(np.pi/2-phi),np.sin(np.pi/2 - theta)*np.sin(np.pi/2-phi),np.cos(np.pi/2-phi)]
-        
+
         if i < segments_fixed:
             camera_set['Fixed_%03i'%(i)]=(pos,vc,False)
         else:
@@ -144,11 +144,11 @@ def generate_cameras(normal_vector, seed = 0, distance=100.0, fov=50.0, mov_ang 
 
     if movie: camera_set['Movie_angle'] = ([0.,math.cos(mov_ang*180./pi),math.sin(2*180./pi)],[0.,0.,-1.],False)
 
-    i=0  
+    i=0
     cameras = OrderedDict()
     for name,(normal,north,do_rot)  in camera_set.items():
         print( name, normal, north, do_rot)
-        
+
         orient = Orientation(normal_vector=normal, north_vector=north)
         if do_rot:
             drot = R.copy()
@@ -164,9 +164,9 @@ def generate_cameras(normal_vector, seed = 0, distance=100.0, fov=50.0, mov_ang 
         else:
             sunrise_up  = normal_vector.copy()
 
-            
+
         if np.all(np.abs(sunrise_up-sunrise_pos)<1e-3):
-            sunrise_up[0] *= 0.5 
+            sunrise_up[0] *= 0.5
         sunrise_direction = -1.0*sunrise_pos
         sunrise_afov = 2.0*np.arctan((fov/2.0)/distance)
         norm = lambda x: x/np.sqrt(np.sum(x*x))
@@ -174,7 +174,7 @@ def generate_cameras(normal_vector, seed = 0, distance=100.0, fov=50.0, mov_ang 
             sunrise_up[0]*=0.5
             sunrise_up = norm(sunrise_up)
         line = (distance*sunrise_pos, distance*sunrise_direction, sunrise_up,
-                sunrise_afov, fov, distance) 
+                sunrise_afov, fov, distance)
         cameras[name] = line
         i+=1
 
@@ -188,7 +188,7 @@ def write_cameras(prefix, cameras):
     for name,row in cameras.items():
         campos += (tuple(row[1])+tuple(row[0])+tuple(row[2])+tuple([row[3]]),)
     campos = np.array(campos)
-    np.savetxt(fn, campos)   
+    np.savetxt(fn, campos)
     fn =  prefix+'.camnames'
     fh = open(fn,'w')
     fh.write('\n'.join([c for c in cameras.keys()]))
@@ -202,21 +202,21 @@ def export_fits(ds, center, export_radius, prefix, star_particles, max_level=Non
     import sunrise_octree_exporter
 
     print( "\nExporting data in %s to FITS for Sunrise"%ds.parameter_filename.split('/')[-1])
-    
+
     filename = prefix+'.fits'
     center = center.in_units('kpc')
     width = export_radius.in_units('kpc')
     info = {}
-    
+
     fle, fre, ile, ire, nrefined, nleafs, nstars, output, output_array = \
 	                                                                 sunrise_octree_exporter.export_to_sunrise(ds, filename, star_particles,  center, width, max_level=max_level, grid_structure_fn = prefix+'_grid_struct.npy', no_gas_p = no_gas_p, form=form)
-    
+
     info['export_ile']=ile
     info['export_ire']=ire
     info['export_fle']=fle
     info['export_fre']=fre
-    
-    
+
+
     info['export_center']=center.value
     info['export_radius']=width.value
     info['export_max_level']=max_level
@@ -224,7 +224,7 @@ def export_fits(ds, center, export_radius, prefix, star_particles, max_level=Non
     info['export_nrefined']=nrefined
     info['export_nleafs']=nleafs
     info['input_filename']=filename
-    
+
     print( "Successfully generated FITS for snapshot %s"%ds.parameter_filename.split('/')[-1])
     print( info,'\n')
     #return info,  output, output_array
@@ -237,16 +237,16 @@ def write_qsub_exporters(snapname,qsubfn,aname,args):
         en = args['email']
     else:
         en = 'gsnyder@stsci.edu'
-        
-    
+
+
     if args['format']=='ENZO':
         code='enzoToSunrise.py'
         group=args['group']
     elif args['format']=='VELA':
         code='genSunriseInput.py'
         group=None
-    
-    
+
+
     qsfo = open(qsubfn,'w')
     qsfo.write('#!/bin/bash\n')
     qsfo.write('#PBS -S /bin/bash\n')
@@ -261,10 +261,10 @@ def write_qsub_exporters(snapname,qsubfn,aname,args):
     qsfo.write('#PBS -o sunrise_export_'+aname+'pbs.out\n')
     qsfo.write('#PBS -e sunrise_export_'+aname+'pbs.err\n')
     qsfo.write('#PBS -V\n\n')
-    
+
     qsfo.write('python $VELAYTSUNRISE_CODE/'+code+' '+os.path.basename(snapname)+' --format='+str(args['format'])+' > export_test_'+aname+'.out 2> export_test_'+aname+'.err\n')
     qsfo.close()
-    
+
 
     return
 
@@ -277,11 +277,11 @@ if __name__ == "__main__":
     import yt
     import sunrise_octree_exporter
     reload(sunrise_octree_exporter)
-    
-    
+
+
     print( args)
     print( args['no_export'], args['no_gas_p'])
-    
+
     if args['snap_files'] is not None:
         snaps = [args['snap_files']]
     else:
@@ -291,20 +291,20 @@ if __name__ == "__main__":
         segments_random=int(args['segments_random'])
     else:
         segments_random=15
-        
+
     print( "Generating Sunrise Input for: ", snaps)
 
     abssnap = os.path.abspath(snaps[0])
 
-    
+
     assert os.path.lexists(abssnap)
-    
+
     dirname = os.path.dirname(abssnap)
     simname = os.path.basename(dirname) #assumes directory name for simulation name
     print( "Simulation name:  ", simname)
 
 
-    
+
     particle_headers = []
     particle_data = []
     stars_data = []
@@ -315,10 +315,10 @@ if __name__ == "__main__":
         particle_data.append('PMcrs0'+aname+'.DAT')
         stars_data.append('stars_'+aname+'.dat')
         snap_dir = os.path.join(simname+'_'+aname+'_sunrise')
-        
+
         print( "Sunrise directory: ", snap_dir)
         if not os.path.lexists(snap_dir):
-            os.mkdir(snap_dir)        
+            os.mkdir(snap_dir)
 
         newf = os.path.join(snap_dir,sn)
         new_snapfiles.append(newf)
@@ -337,20 +337,20 @@ if __name__ == "__main__":
 
 
     #gen_name, gal_name, snap_name, snaps  = 'VELA_v2', 'VELA27', 'VELA27_a0.370', '../data/VELA27_v2/a0.370/10MpcBox_csf512_a0.370.d'
-        
+
     #snap_dir = '/Volumes/wd/yt_pipeline/Runs/%s/%s/%s'%(gen_name, gal_name, snap_name+'_sunrise')
 
     #assert os.path.exists(snap_dir), 'Snapshot directory %s not found'%snap_dir
 
-        
-        
+
+
     a = time.time()
 
 
     galprops_file = simname+'_galprops.npy'
     galprops = np.load(galprops_file)[()]
 
-    
+
     cam_dist = 100000
 
     max_level = None
@@ -361,20 +361,20 @@ if __name__ == "__main__":
 
 
     #ts = yt.DatasetSeries(new_snapfiles)
-        
-    # Loop over snapshot to generate cameras and projection plots, 
+
+    # Loop over snapshot to generate cameras and projection plots,
     # parallelization happens while generating the plots.
     for snapfile in new_snapfiles:
 
         aname = (os.path.basename(snapfile)).split('_')[-1].rstrip('.d')
-        
+
         print( "Timestep name: ", aname)
 
         snap_dir = os.path.dirname(snapfile) #os.path.join(simname+'_'+aname+'_sunrise')
 
         print( "Sunrise directory: ", snap_dir)
         assert os.path.lexists(snap_dir)
-                   
+
 
         out_dir = os.path.join(snap_dir,'input')
         print( os.path.lexists(out_dir))
@@ -391,12 +391,12 @@ if __name__ == "__main__":
         except:
             print('Snapfile exists in galprops but data does not, skipping...')
             continue
-            
+
 
         cam_fov = galprops['fov_kpc'][idx]
 
 
-        
+
         #scale = round(1.0/(ds.current_redshift+1.0),4)
         #if scale not in galprops['scale']: continue
         #idx = np.argwhere(galprops['scale'] == scale)[0][0]
@@ -404,14 +404,14 @@ if __name__ == "__main__":
         stars_L = galprops['stars_L'][idx]
         gas_L 	= galprops['gas_L'][idx]
 
-        
+
         try:
             L_sum = stars_L + gas_L
         except TypeError:
             L_sum = gas_L
 
         L = L_sum/np.sqrt(np.sum(L_sum*L_sum))
-        
+
 
         #L_temp = array([0.229307690083501, 0.973325655982054, 0.00742635009091421]) #to Match with C Moody
         #This function is important for generating the cameras that we will be using
@@ -432,7 +432,7 @@ if __name__ == "__main__":
 
 
     eaf.close()
-    
+
     if args['no_export'] is True:
         print( "Skipping export stage, per command argument.")
         exit()
@@ -440,85 +440,53 @@ if __name__ == "__main__":
     print( "Continuing to export grids.")
 
     yt.enable_parallelism()
-    
+
     ts = yt.DatasetSeries(new_snapfiles)
 
-    # Send one snapshots to each processor to export 
+    # Send one snapshots to each processor to export
 
     for ds in ts.piter():
         aname = (os.path.basename(ds._file_amr)).split('_')[-1].rstrip('.d')
         print( "Timestep name: ", aname)
         snap_dir = os.path.dirname(ds._file_amr)
         assert os.path.lexists(snap_dir)
-        
+
         out_dir = os.path.join(snap_dir, 'input')
         assert os.path.lexists(out_dir)
-        
+
         prefix = os.path.join(out_dir,simname+'_'+aname)
-        
+
         snapfile = ds._file_amr
-        
+
         if os.path.abspath(snapfile) not in galprops['snap_files']: continue
         idx = np.argwhere(galprops['snap_files']==os.path.abspath(snapfile))[0][0]
-        
+
         scale = round(1.0/(ds.current_redshift+1.0),4)
         #idx = np.argwhere(galprops['scale'] == scale)[0][0]
-	
+
         gal_center = galprops['true_center'][idx]
         gal_center = ds.arr(gal_center, 'kpc')
-        
+
         #export_radius = ds.arr(max(1.2*cam_dist, 1.2*cam_fov), 'kpc')
         export_radius = ds.arr(1.2*cam_fov, 'kpc')
-        
+
         print( export_radius)
-        
-        export_info = export_fits(ds, gal_center, export_radius, 
-                                  prefix, star_particles = 'stars', 
+
+        export_info = export_fits(ds, gal_center, export_radius,
+                                  prefix, star_particles = 'stars',
                                   max_level=max_level, no_gas_p = args['no_gas_p'])
 
         export_info['sim_name'] = simname
         export_info['scale'] = scale
         export_info['snap_file'] = snapfile
-        
+
         export_info_file = prefix + '_export_info.npy' #galprops_file.replace('galprops', 'export_info')
         np.save(export_info_file, export_info)
         sys.stdout.flush()
 
         #update star radii
-        update_star_radius.update_star_radius(export_info['input_filename'])
-        
+        #commenting out to use rad=0.01 kpc default for each star -- preserves maximum structure in SF regions.  May revisit.
+        #update_star_radius.update_star_radius(export_info['input_filename'])
+
     b = time.time()
     print( 'Final time in seconds: ', b - a)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
