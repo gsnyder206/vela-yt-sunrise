0b096308eb82e6f8a8fce22eab01c46917f6a728
gsnyder206 <gsnyder@stsci.edu>
2021-08-11 14:06:34 -0400

    new camera untar script

---
+++ untar_hlsp_by_camera.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import numpy as np
+import glob
+import shutil
+import tarfile
+import string
+#import pandas
+import astropy
+import astropy.cosmology
+import astropy.io.fits as pyfits
+import astropy.io.ascii as ascii
+import subprocess
+
+
+if __name__=="__main__":
+
+    #run from HLSP directory under simulation e.g. vela06
+
+    dirname=os.path.basename(os.path.abspath('.'))  #e.g. vela06
+
+    cams=np.sort(np.asarray(glob.glob('vela??_cam??.tar')))
+    print(dirname)
+    print(cams)
+
+    for camst in cams:
+        print('untarring ', camst)
+        subprocess.call(['tar','xf',dirname+'_'+camst+'.tar'])
